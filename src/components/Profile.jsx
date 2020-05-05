d3ee6f063125f3339194a0cbf85b11bb9bcce865
Andrei Artemchuk <and64region2015@gmail.com>
2020-05-05 12:33:24 +0300

    react-it-kama

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'/>
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+
+};
+
+export default Profile;
