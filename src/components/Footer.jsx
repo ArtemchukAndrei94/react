a0638bead8ea9d49ca9418e5da78fcd93f63e657
Andrei Artemchuk <and64region2015@gmail.com>
2020-05-06 12:59:39 +0300

    14-23

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Footer.module.css';
 
 const Footer = () => {
     return (
-        <div className='footer'>
+        <div className={classes.footer}>
             <h3>Footer</h3>
         </div>
     );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.profile}>
             <div>
                 <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'/>
             </div>
